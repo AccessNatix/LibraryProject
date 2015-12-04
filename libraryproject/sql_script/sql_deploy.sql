227562e174c0c68d12cc02e1b60b0c0129871c2d
AccessNatix <anatole.curlier@gmail.com>
2015-12-04 16:15:54 +0100

    add sql deploy

--- libraryproject/sql_script/sql_deploy.sql
+++ libraryproject/sql_script/sql_deploy.sql
@@ -8,8 +8,46 @@
  * Created: 30 nov. 2015
  */
 
-CREATE TABLE STUDENT(
-    NAME VARCHAR(20) NOT NULL,
-    SURNAME VARCHAR(20) NOT NULL,
-    AGE  INT NOT NULL
+
+drop table if EXISTS `book`;
+drop table if EXISTS `author`;
+drop table if EXISTS `category`;
+drop table if EXISTS `user`;
+
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45),
+  `surname` varchar(45),
+  `mail` varchar(45) NOT NULL UNIQUE,
+  `username` varchar(45) NOT NULL UNIQUE,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
+create table IF NOT EXISTS `author`(
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(16),
+    PRIMARY KEY(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `book` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `price` decimal(6,2) NOT NULL,
+  `description` tinytext NOT NULL,
+  `category_id` int(10) unsigned NOT NULL,
+  `stock` int(10) unsigned NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `author_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`category_id`) REFERENCES `category`(`id`),
+  FOREIGN KEY (`author_id`) REFERENCES `author`(`id`)
 );
